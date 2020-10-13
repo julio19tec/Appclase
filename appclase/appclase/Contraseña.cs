3348e1cbdfe8fda3ed4f69fd728b2e4397c6e380
julio19tec <71047390+julio19tec@users.noreply.github.com>
2020-10-12 21:40:38 -0500

    Add files via upload

---
+++ appclase/appclase/Contraseña.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appclase
+{
+    class Contraseña
+    {
+        //variables
+        string pin;
+        string usuario;
+
+
+        public string Pin
+        {
+            get
+            { 
+                return pin;
+            }
+            set 
+            { 
+                pin = value; 
+            }
+        }
+        public string Usuario
+        {
+            get
+            {
+                return usuario;
+            }
+            set
+            {
+                usuario = value;
+            }
+        }
+
+
+        //constructores 
+        public Contraseña()
+        {
+            pin = "";
+            usuario = "";
+        }
+        public Contraseña(string pin, string usuario)
+        {
+            this.pin = pin;
+            this.usuario = usuario;
+        }
+
+        public override string ToString()
+        {
+            return "Pin" + pin + "usuario" + usuario;
+        }
+
+
+
+    }
+}
---
+++ appclase/appclase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appclase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contraseña miContraseña;
+            miContraseña = new Contraseña("16092001", "julio");
+            Console.WriteLine(miContraseña.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
